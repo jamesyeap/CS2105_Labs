3c81930ff21d860bc605d65deebdbc0f653f7bec
James Yeap <jamesyeap.ys@gmail.com>
2022-02-07 22:34:04 +0800

    Add new files

--- assignment_1/Hacker-A0218234L.py
+++ assignment_1/Hacker-A0218234L.py
@@ -108,11 +108,6 @@ precomputed_passwords = []
 for i in range(0, 10000):
 	precomputed_passwords.append(str(i).encode().rjust(4, b'0'))
 
-# pre-computed login requests
-precomputed_login_requests = []
-for i in range(0, 10000):
-	precomputed_login_requests.append(LOGIN_REQUEST + precomputed_passwords[i])
-
 # request for connection to server
 clientSocket = create_socket(student_key)
 
@@ -125,11 +120,9 @@ current_password = 0
 # try to login using all possible password combinations (0000-9999)
 while (num_success < 8 and current_password < 10000):
 	# padded_password = str(current_password).encode().rjust(4, b'0')
-	# padded_password = precomputed_passwords[current_password]
+	padded_password = precomputed_passwords[current_password]
 
-	# can_login = request_login(clientSocket, padded_password)
-	clientSocket.send(precomputed_login_requests[current_password])
-	can_login = get_response_code(clientSocket) == LOGIN_SUCCESSFUL
+	can_login = request_login(clientSocket, padded_password)
 
 	if (can_login):
 		target_file = request_get_file(clientSocket)
@@ -137,7 +130,7 @@ while (num_success < 8 and current_password < 10000):
 		request_validate_hash(clientSocket, md5_hash)
 
 		num_success += 1
-		# print(padded_password)
+		print(padded_password)
 		request_logout(clientSocket)
 
 	current_password += 1
