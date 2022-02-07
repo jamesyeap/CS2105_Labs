95ae1cca57e98fbdb13e1f7979f8b772b0280753
James Yeap <jamesyeap.ys@gmail.com>
2022-02-07 15:52:08 +0800

    Add new files

--- assignment_1/Hacker-A0218234L.py
+++ assignment_1/Hacker-A0218234L.py
@@ -124,11 +124,15 @@ last_tried_password = 0 # all possible password combinations (0000-9999)
 clientSocket = create_socket(student_key)
 
 while (num_success < 8):
+	request_logout(clientSocket)
+
+	exit(0)
+
 	# try to login using all possible password combinations (0000-9999)
 	for i in range(last_tried_password, 10000):
 		try:
 			can_login = request_login(clientSocket, i)
-			print(i) # FOR DEBUGGING ONLY: see how far we could get
+			print("--tried password: " + str(i)) # FOR DEBUGGING ONLY: see how far we could get
 
 			if (can_login):
 				target_file = request_get_file(clientSocket)
