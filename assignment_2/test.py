196b4ab609e5defd8529834389b4ee0f41368756
James Yeap <jamesyeap.ys@gmail.com>
2022-03-08 21:43:18 +0800

    Add new files

--- assignment_2/test.py
+++ assignment_2/test.py
@@ -2,6 +2,6 @@ from socket import *
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect(("137.132.92.111", 4445))
-s.sendall("STID_651723_C".encode())
+s.sendall("STID_A0218234L_C".encode())
 resp = s.recv(4096).decode()
 print(f"resp = {resp}")
