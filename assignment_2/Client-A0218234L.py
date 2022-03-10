19bd5d011ccab28fec16cb5edf4d7d6c3777c9a0
James Yeap <jamesyeap.ys@gmail.com>
2022-03-10 19:05:35 +0800

    Add new files

--- assignment_2/Client-A0218234L.py
+++ assignment_2/Client-A0218234L.py
@@ -84,6 +84,10 @@ def deliver(data, receiver):
 def receive_packet(socket, receiver, expectedSeqNum):
 	packetSeqNum, seqNumLength = extract_packet_seqnum(socket);
 	packetCheckSum, checkSumLength = extract_packet_checksum(socket);
+
+	print("[PACKET - SEQNUM - LENGTH]: " + str(seqNumLength));
+	print("[PACKET - CHECKSUM - LENGTH]: " + str(checkSumLength));
+	
 	packetData, num_bytes_received = extract_packet_data(socket, 1024 - seqNumLength - checkSumLength);
 
 	if (is_not_corrupted(packetData, packetCheckSum)):
