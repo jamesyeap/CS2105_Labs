be481b6a0ffeae9429de6516f4d0ac0ddcecfb41
James Yeap <jamesyeap.ys@gmail.com>
2022-03-10 21:24:15 +0800

    Add new files

--- assignment_2/Client-A0218234L.py
+++ assignment_2/Client-A0218234L.py
@@ -33,6 +33,8 @@ MAX_PACKET_SIZE = 1024;
 def get_packet_header_seqnum(socket):
 	data = socket.recv(PACKET_HEADER_SEQNUM_SIZE);
 
+	print("[NUM BYTES RECEIVED FOR SEQNUM]: "+ str(len(data)));
+
 	if (len(data) == 0):
 		return (None, True);
 
