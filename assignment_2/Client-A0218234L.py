70b33ccc8087b27d6605dd1cde9095f4f15ee72e
James Yeap <jamesyeap.ys@gmail.com>
2022-03-13 02:57:21 +0800

    Add new files

--- assignment_2/Client-A0218234L.py
+++ assignment_2/Client-A0218234L.py
@@ -61,7 +61,7 @@ def get_message_until_size_reached(socket, total_length):
 def get_packet_seqnum(socket):
 	seqnum_inbytes = get_message_until_size_reached(socket, PACKET_HEADER_SEQNUM_SIZE);
 
-	# print("[seqnum]: " + seqnum_inbytes.decode());
+	print(seqnum_inbytes);
 
 	return seqnum_inbytes;
 
@@ -80,16 +80,16 @@ def get_packet_length(socket):
 	return packet_length_inbytes;
 
 def get_packet_header(socket):
-	seqnum = int(get_packet_seqnum(socket).decode());
-	checksum = int(get_packet_checksum(socket).decode());
-	data_payload_length = int(get_packet_length(socket).decode());
+	seqnum_inbytes = get_packet_seqnum(socket);
+	checksum_inbytes = get_packet_checksum(socket);
+	data_payload_length_inbytes = get_packet_length(socket);
 
-	return seqnum, checksum, data_payload_length;
+	return seqnum_inbytes, checksum_inbytes, data_payload_length_inbytes;
 
 def get_packet(socket):
-	seqnum, checksum, data_payload_length = get_packet_header(socket);
+	seqnum_inbytes, checksum_inbytes, data_payload_length_inbytes = get_packet_header(socket);
 
-	if (data_payload_length == 0):
+	if (data_payload_length_inbytes == b'0000'):
 		return None;
 	else:
 		packet_data = get_message_until_size_reached(socket, data_payload_length);
