380413d871645ea833aa53404077e8dd0a716f30
James Yeap <jamesyeap.ys@gmail.com>
2022-03-13 00:28:39 +0800

    Add new files

--- assignment_2/Client-A0218234L.py
+++ assignment_2/Client-A0218234L.py
@@ -87,7 +87,7 @@ def get_packet_header(socket):
 	return seqnum, checksum, data_payload_length;
 
 def get_packet(socket):
-	indicator = get_packet_header_indicator();
+	indicator = get_packet_header_indicator(socket);
 
 	if (indicator == PACKET_HEADER_INDICATOR_INCOMING_PACKET):
 		seqnum, checksum, data_payload_length = get_packet_header();
