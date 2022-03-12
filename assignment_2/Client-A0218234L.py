b145f2dbcb02801505dcf2aad8afdc002625eae0
James Yeap <jamesyeap.ys@gmail.com>
2022-03-13 01:09:33 +0800

    Add new files

--- assignment_2/Client-A0218234L.py
+++ assignment_2/Client-A0218234L.py
@@ -179,7 +179,9 @@ clientSocket.close();
 	=============================================================================================
 - my student-key is 651723
 - to test the code:
-	python3 Client-A0218234L.py 651723 0 137.132.92.111 4445 output.txt
+	python3 Client-A0218234L.py 651723 0 137.132.92.111 4445 myownoutput.txt
+
+	python3 Server-A0218234L.py 651723 0 137.132.92.111 4445 myowninput.txt
 - to run the reliable channel:
 	On Terminal 1: (Client) ./test/FileTransfer.sh -i 651723 -n
 	On Terminal 2: (server) ./test/FileTransfer.sh -s -i 651723 -n
