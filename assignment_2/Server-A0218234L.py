6168d3d99a2968c37e977e73702412a4e25fa4c9
James Yeap <jamesyeap.ys@gmail.com>
2022-03-13 16:44:43 +0800

    Add new files

--- assignment_2/Server-A0218234L.py
+++ assignment_2/Server-A0218234L.py
@@ -24,7 +24,7 @@ def wait_for_turn(socket):
 		if (queue_len == b'0_'):
 			break;
 
-		print(queue_len); # TO REMOVE
+		print("[POSITION IN QUEUE]: " + str(queue_len)); # TO REMOVE
 		queue_len = get_response_message(socket);
 
 # ----- GENERATE PACKET FUNCTIONS --------------------------------------------
